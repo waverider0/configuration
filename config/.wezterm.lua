b9b610511015d6c46aa709df1bd43e421b86c9d0
allen <waverider0@users.noreply.github.com>
2025-08-13 17:10:57 -0400

    easier on the eyes

--- config/.wezterm.lua
+++ config/.wezterm.lua
@@ -1,8 +1,8 @@
 local wezterm = require "wezterm"
 
 local config = {
-	colors = { background = '#1e1e1e' },
-	font_size = 10.0
+	colors = { background = '#151515' },
+	font_size = 12
 }
 
 if wezterm.target_triple:find("windows") then

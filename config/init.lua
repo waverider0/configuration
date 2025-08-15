e43293a0600d989d6411b45cf79f4dab08c013cb
allen <waverider0@users.noreply.github.com>
2025-08-15 19:20:26 -0400

    .

--- config/init.lua
+++ config/init.lua
@@ -22,12 +22,9 @@ vim.cmd("autocmd FileType * setlocal noexpandtab tabstop=2 shiftwidth=2 softtabs
 vim.keymap.set("i"       , "<C-h>"   , "<C-w>") -- most terminals interpret ctrl-h as ctrl-backspace
 vim.keymap.set("i"       , "<C-Del>" , "<C-o>dw")
 vim.keymap.set("i"       , "<C-c>"   , "<Esc>")
-vim.keymap.set("n"       , "<C-b>"   , "<C-v>")
 vim.keymap.set("n"       , "<C-c>"   , function() if vim.v.hlsearch == 1 then vim.cmd("nohlsearch") end end)
 vim.keymap.set("n"       , "<C-e>"   , "<nop>")
 vim.keymap.set("n"       , "Q"       , "<nop>")
 vim.keymap.set("n"       , "gb"      , "<cmd>pop<CR>")
 vim.keymap.set("n"       , "gd"      , "<C-]>")
-vim.keymap.set({"n","v"} , "<C-d>"   , "<C-d>zz")
-vim.keymap.set({"n","v"} , "<C-u>"   , "<C-u>zz")
 vim.keymap.set({"n","v"} , "s"       , "<nop>")

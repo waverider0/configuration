192cd6932f73662f3056b07b5a1f0cfcd43470db
allen <waverider0@users.noreply.github.com>
2025-10-24 20:22:27 -0400

    silent

--- config/nvim_init.lua
+++ config/nvim_init.lua
@@ -2,7 +2,7 @@ vim.cmd('autocmd FileType * setlocal shiftwidth=2 tabstop=2 softtabstop=2 expand
 vim.cmd "colorscheme unokai"
 vim.cmd "syntax on"
 
-vim.keymap.set({"n","v"}, "<C-c>", ":nohlsearch<CR>")
+vim.keymap.set({"n","v"}, "<C-c>", ":nohlsearch<CR>", { silent = true })
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
 vim.opt.autoindent = true

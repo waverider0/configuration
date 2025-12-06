dd3131fa699f4859576f2e8a94f1502eea6093c2
allen <waverider0@users.noreply.github.com>
2025-12-06 18:36:31 -0500

    nixos -> arch pt1
    too much dev friction on nix (esp w python)

--- deploy.py
+++ deploy.py
@@ -1,39 +1,36 @@
 #!/usr/bin/env python3
 
 import os, shutil
-import secret_manager as sm
+import secret_manager
 
 HOME = os.path.expanduser("~")
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
+dotfiles = {
+  "dotfiles/.gitconfig": f"{HOME}/.gitconfig",
+  "dotfiles/github.pub": f"{HOME}/.ssh/github.pub",
+  "dotfiles/ssh_config": f"{HOME}/.ssh/config",
+}
+
+secrets = (
+  f"{HOME}/.kdbx.kdbx",
+  f"{HOME}/.ssh/github",
+)
+
 if os.name == "nt":
-  dotfiles = {
-    "dotfiles/.gitconfig"              : f"{HOME}/.gitconfig",
-    "dotfiles/github.pub"              : f"{HOME}/.ssh/github.pub",
-    "dotfiles/vscode_keybindings.json" : f"{HOME}/AppData/Roaming/VSCodium/User/keybindings.json",
-    "dotfiles/vscode_settings.json"    : f"{HOME}/AppData/Roaming/VSCodium/User/settings.json",
-    "dotfiles/ssh_config"              : f"{HOME}/.ssh/config",
-    "dotfiles/windows/startup.bat"     : "C:/ProgramData/Microsoft/Windows/Start Menu/Programs/Startup/startup.bat",
-  }
-  secrets = (
-    f"{HOME}/.kdbx.kdbx",
-    f"{HOME}/.ssh/github",
-  )
+  dotfiles["dotfiles/vscode_keybindings.json"] = f"{HOME}/AppData/Roaming/VSCodium/User/keybindings.json"
+  dotfiles["dotfiles/vscode_settings.json"]    = f"{HOME}/AppData/Roaming/VSCodium/User/settings.json"
+  dotfiles["dotfiles/windows/startup.bat"]     = "C:/ProgramData/Microsoft/Windows/Start Menu/Programs/Startup/startup.bat" # requires admin
+elif os.name == "posix":
+  dotfiles["dotfiles/nvim_init.lua"]            = f"{HOME}/.config/nvim/init.lua"
+  dotfiles["dotfiles/vscode_keybindings.json"]  = f"{HOME}/.config/VSCodium/User/keybindings.json"
+  dotfiles["dotfiles/vscode_settings.json"]     = f"{HOME}/.config/VSCodium/User/settings.json"
+  dotfiles["dotfiles/unix/.tmux.conf"]          = f"{HOME}/.tmux.conf"
+  dotfiles["dotfiles/unix/.zshrc"]              = f"{HOME}/.zshrc" # chsh -s /bin/zsh
+  #dotfiles["dotfiles/unix/linux/rofi"]          = f"{HOME}/.config/rofi/config.rasi"
+  #dotfiles["dotfiles/unix/linux/hyprland.conf"] = f"{HOME}/.config/hypr/hyprland.conf"
 else:
-  dotfiles = {
-    "dotfiles/.gitconfig"              : f"{HOME}/.gitconfig",
-    "dotfiles/nvim_init.lua"           : f"{HOME}/.config/nvim/init.lua",
-    "dotfiles/github.pub"              : f"{HOME}/.ssh/github.pub",
-    "dotfiles/vscode_keybindings.json" : f"{HOME}/.config/VSCodium/User/keybindings.json",
-    "dotfiles/vscode_settings.json"    : f"{HOME}/.config/VSCodium/User/settings.json",
-    "dotfiles/ssh_config"              : f"{HOME}/.ssh/config",
-    "dotfiles/unix/.tmux.conf"         : f"{HOME}/.tmux.conf",
-    "dotfiles/unix/.zshrc"             : f"{HOME}/.zshrc",
-  }
-  secrets = (
-    f"{HOME}/.kdbx.kdbx",
-    f"{HOME}/.ssh/github",
-  )
+  raise NotImplementedError(f"{os.name} is not supported")
 
 if __name__ == "__main__":
   for name, dest in dotfiles.items():
@@ -43,15 +40,15 @@ if __name__ == "__main__":
     shutil.copy(src, dest)
     print(f"copied -> {dest}")
 
-  sm.decrypt_secrets()
+  secret_manager.decrypt_secrets()
   for dest in secrets:
-    src = os.path.join(sm.SECRETS_DIR, os.path.basename(dest))
+    src = os.path.join(secret_manager.SECRETS_DIR, os.path.basename(dest))
     if not os.path.exists(src): continue
     os.makedirs(os.path.dirname(dest), exist_ok=True)
     shutil.copy(src, dest)
     print(f"copied -> {dest}")
-  shutil.rmtree(sm.SECRETS_DIR)
-  print(f"deleted: {sm.SECRETS_DIR}")
+  shutil.rmtree(secret_manager.SECRETS_DIR)
+  print(f"deleted: {secret_manager.SECRETS_DIR}")
 
   print("Done!")
 
